4970090f8e8fc679e51d7ff2ab7dfc2352d38182
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2026-06-27 21:00:14 +0330

    update all file and create file dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import type { Task } from './task.module';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get('/:id')
+  getTaskOne(@Param('id') id: string) {
+    return `${uuid()} and ${id}`;
+  }
+}

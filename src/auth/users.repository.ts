ddc127ee78f619387c26433c509f8200a918c2a9
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2026-07-02 00:51:13 +0330

    impelement user.repository.ts

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly repository: Repository<User>,
+  ) {}
+}

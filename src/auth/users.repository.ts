e989f86dfc6e38dc2cacd52329126b8f694d50dd
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2026-07-02 01:15:25 +0330

    create authCredentialsdto  for create user and impelemnet createUser in repository

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { Injectable } from '@nestjs/common';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -9,4 +10,10 @@ export class UserRepository {
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentilasDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentilasDto;
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+  }
 }
